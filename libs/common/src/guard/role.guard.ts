70c580314eb26b646379101ad137be28262722e7
Oleksandr Kuzmenko <eight.alex@gmail.com>
2024-06-13 20:27:12 +0300

    presave before role/permission start

--- libs/common/src/guard/role.guard.ts
+++ libs/common/src/guard/role.guard.ts
@@ -4,7 +4,6 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
 @Injectable()
-// export class JwtRoleGuard extends AuthGuard(AUTH_JWT_ACCESS_TYPE) {
 export class RoleGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
   canActivate(context: ExecutionContext): boolean {
