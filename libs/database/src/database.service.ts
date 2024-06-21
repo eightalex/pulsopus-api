be854ee7ff13e466bbbfa8db6da71d051f103666
Oleksandr Kuzmenko <eight.alex@gmail.com>
2024-06-21 11:47:46 +0300

    fix(hexbin): add rate/trend data

--- libs/database/src/database.service.ts
+++ libs/database/src/database.service.ts
@@ -40,15 +40,18 @@ export class DatabaseService {
         const cD = moment(Number(act.date)).startOf('day').valueOf();
         const pD = moment(cD).subtract(diff, 'day').startOf('day').valueOf();
         const nD = moment(cD).add(diff, 'day').startOf('day').valueOf();
-        currAct.push(act);
-        prevAct.push(Activity.of(pD, act.value ? act.value * 0.3 : 0));
-        nextAct.push(Activity.of(nD, act.value ? act.value * 1.3 : 0));
+        const cV = Number(act.value) || 0;
+        const pV = cV * 0.8;
+        const nV = cV * 0.3;
+        currAct.push(Activity.of(cD, cV));
+        prevAct.push(Activity.of(pD, pV));
+        nextAct.push(Activity.of(nD, nV));
       }
       const activity = [...prevAct, ...currAct, ...nextAct];
       return new User({ ...u, activity });
     });
     //
-    const designDepartmentUsers = [...csvusrs].splice(0, 4).map((u) => {
+    const designDepartmentUsers = [...csvusrs].map((u) => {
       return new User({
         ...u,
         id: uuidv4(),
@@ -58,11 +61,11 @@ export class DatabaseService {
         activity: u.activity
           .filter((a) => !!a && !!a.date)
           .map((a) => {
-            return Activity.of(a.date, a.value * 0.3);
+            return Activity.of(a.date, Number(a.value) * 0.3);
           }),
       });
     });
-    const productDepartmentUsers = [...csvusrs].splice(4).map((u) => {
+    const productDepartmentUsers = [...csvusrs].map((u) => {
       return new User({
         ...u,
         id: uuidv4(),
@@ -72,7 +75,7 @@ export class DatabaseService {
         activity: u.activity
           .filter((a) => !!a && !!a.date)
           .map((a) => {
-            return Activity.of(a.date, a.value * 0.2);
+            return Activity.of(a.date, Number(a.value) * 0.2);
           }),
       });
     });
@@ -101,7 +104,7 @@ export class DatabaseService {
   }
 
   public computeData() {
-    const companyActivityMap: Map<string, number> = new Map();
+    const OLD_companyActivityMap: Map<string, number> = new Map();
     for (const [userId, user] of this.usersMap) {
       user.department =
         this.departmentsValuesMap.get(user.department?.value) || undefined;
@@ -109,46 +112,59 @@ export class DatabaseService {
       //
       const cmp = this.departmentsValuesMap.get(EDepartment.COMPANY);
       cmp.users.push(user);
+      this.departmentsValuesMap.set(EDepartment.COMPANY, cmp);
       //
       if (user.department) {
         user.department.users.push(user);
       }
 
       user.activity.forEach(({ date, value }) => {
-        const compDayAct = companyActivityMap.get(date) || 0;
-        companyActivityMap.set(date, compDayAct + Number(value) || 0);
+        const d = Number(date).toString();
+        const compDayAct = OLD_companyActivityMap.get(d) || 0;
+        const newValue = Number(compDayAct) + Number(value) || 0;
+        OLD_companyActivityMap.set(d, newValue);
       });
     }
 
-    const actMap = this.departmentsValuesMap
-      .get(EDepartment.COMPANY)
-      .activity.reduce((m, { date, value }) => {
-        m.set(date, value);
-        return m;
-      }, new Map());
+    const departmentActivitiesMap: Map<
+      EDepartment,
+      Map<string, number>
+    > = new Map();
+    for (const d of this.departmentsValuesMap.values()) {
+      const activitiesMap =
+        departmentActivitiesMap.get(d.value) || new Map<string, number>();
+      for (const user of d.users) {
+        for (const { date, value } of user.activity) {
+          const d = Number(date).toString();
+          const prevValue = activitiesMap.get(d) || 0;
+          const nextValue = prevValue + Number(value);
+          activitiesMap.set(d, nextValue);
+        }
+      }
+      departmentActivitiesMap.set(d.value, activitiesMap);
+    }
+    const companyActivityMap = departmentActivitiesMap.get(EDepartment.COMPANY);
 
     for (const d of this.departmentsValuesMap.values()) {
-      const depActMap: Map<string, number> = new Map();
       d.users = d.users.map((u) => {
         u.activity = u.activity.map((activity, index, activities) => {
           const { date, value = 0 } = activity;
-          if (!date) {
-            console.log('date', date);
-          }
-          const cmpAct = actMap.get(date) || value;
+          const cmpAct = companyActivityMap.get(date) || value;
           const rate = !value ? 0 : (value / cmpAct) * 100;
           const prevV = !index ? 0 : activities[index - 1].value;
           const trend = value - prevV;
-          const depDayAct = depActMap.get(date) || 0;
-          depActMap.set(date, depDayAct + Number(value) || 0);
           return Activity.of(date, value, rate, trend);
         });
-        this.usersMap.set(u.id, u);
-        return u;
+        const newUser = new User({
+          ...u,
+        });
+        this.usersMap.set(newUser.id, newUser);
+        return newUser;
       });
+      const depActMap = departmentActivitiesMap.get(d.value);
       d.activity = [...depActMap.entries()].map(([date, value]) => {
         const cmpValue = companyActivityMap.get(date) || 0;
-        const rate = !cmpValue || !value ? 0 : (value / cmpValue) * 100;
+        const rate = !(cmpValue && value) ? 0 : (value / cmpValue) * 100;
         return Activity.of(date, value, rate);
       });
       this.departmentsMap.set(d.id, d);
