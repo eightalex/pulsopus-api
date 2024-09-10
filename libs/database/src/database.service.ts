a87de0f856d99f06bd64010ce397133de0ed2a90
Oleksandr Kuzmenko <eight.alex@gmail.com>
2024-09-10 10:36:52 +0300

    feat(api): add restart db from endpoints

--- libs/database/src/database.service.ts
+++ libs/database/src/database.service.ts
@@ -1,4 +1,11 @@
+import { DataLoaderService } from '@app/database/data-loader/data-loader.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService {
+  constructor(private readonly dataLoaderService: DataLoaderService) {}
+
+  public async reset() {
+    await this.dataLoaderService.initial(true);
+  }
+}
